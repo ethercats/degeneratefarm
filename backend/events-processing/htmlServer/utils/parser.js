3ed7deeeb46d72abaa352f6b57d696bf43adc9e5
Woody Deck <Home@Mac-mini.local>
2021-11-25 16:00:40 -0500

    Update parser.js

--- backend/events-processing/htmlServer/utils/parser.js
+++ backend/events-processing/htmlServer/utils/parser.js
@@ -9,7 +9,6 @@ const HTMLTemplate = `<!DOCTYPE html>
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
     <link href="./css/main.css" rel="stylesheet">
 </head>
 
@@ -41,16 +40,55 @@ const HTMLTemplate = `<!DOCTYPE html>
 
 </html>`;
 
+const HTMLTemplateRender = `<!DOCTYPE html>
+<html lang="en" style="height: 100%; width: 100%">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link href="./css/main.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="preloader">
+        <div class="spinner"></div>
+    </div>
+    <div id="wrapper">
+        <div class="nft">
+            <div id="pig-card">
+                <img id="bg" src="">
+                <img id="tail" src="">
+                <img id="ears" src="">
+                <img id="eyes" src="">
+                <img id="body-right-hoof" src="">
+                <img id="teeth" src="">
+                <img id="tongue" src="">
+                <img id="eye-expression" src="">
+                <img id="left-hoof" src="">
+            </div>
+        </div>
+    </div>
+</body>
+<script src="./js/render-image.js"></script>
+<script src="./js/alchemyWeb3.min.js"></script>
+<script src="./js/script.js"></script>
+
+</html>`;
+
 const processMintEvent = async (event) => {
     const tokenId = event.returnValues['tokenId'];
     const filePath = getHTMLPath(tokenId);
     
     await writeFileAsync(filePath, HTMLTemplate);
+    await writeFileAsync(getHTMLRenderPath, HTMLTemplateRender);
     console.log(event.returnValues[2] + ".html has been saved.");
 }
 
 const getHTMLPath = (tokenId) => {
     return path.resolve(__dirname, '..', '..', '..', 'www', 'www', 'nfts', 'pigs', `${tokenId}.html`)
+}
+
+const getHTMLRenderPath = (tokenId) => {
+    return path.resolve(__dirname, '..', '..', '..', 'www', 'www', 'nfts', 'pigs', 'preview-images0', `${tokenId}.html`)
 }
 
 const parseTokenId = (event) => {
